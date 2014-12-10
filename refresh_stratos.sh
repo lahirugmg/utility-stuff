91b01861f3e69bed1dbc36536328bf78c9bcea78
Lahiru Sandaruwan <lahirus@apache.org>
2014-12-10 12:12:53 +0530

    Update refresh_stratos.sh

--- refresh_stratos.sh
+++ refresh_stratos.sh
@@ -8,9 +8,10 @@ cp $path/products/stratos/modules/distribution/target/apache-stratos-4.1.0-SNAPS
 rm -rf apache-stratos-4.1.0-SNAPSHOT
 unzip apache-stratos-4.1.0-SNAPSHOT.zip 
 
+# copy any artifacts you need to update
 cp $path/components/org.apache.stratos.autoscaler/target/org.apache.stratos.autoscaler-4.1.0-SNAPSHOT.jar apache-stratos-4.1.0-SNAPSHOT/repository/components/plugins/org.apache.stratos.autoscaler_4.1.0.SNAPSHOT.jar
+cp $path/products/stratos/modules/distribution/src/main/conf/drools/scaling.drl apache-stratos-4.1.0-SNAPSHOT/repository/conf/drools/scaling.drl
 
-#cp $path/products/stratos/modules/distribution/src/main/conf/drools/scaling.drl apache-stratos-4.1.0-SNAPSHOT/repository/conf/drools/scaling.drl
-
+# put the requied conf files in a directory called "co"
 cp co/* apache-stratos-4.1.0-SNAPSHOT/repository/conf/
 
