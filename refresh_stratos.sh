a63e7d5a73edf56ac5de8c46dc4f8b21a457f96b
Lahiru Sandaruwan <lahirus@apache.org>
2014-12-10 12:12:43 +0530

    Update to use path

--- refresh_stratos.sh
+++ refresh_stratos.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
 
-path = /Users/lahiru/work/apache/stratos/code/master/work/stratos
-cp $path/products/stratos/modules/distribution/target/apache-stratos-4.1.0-SNAPSHOT.zip apache-stratos-4.1.0-SNAPSHOT.zip
+path="/Users/lahiru/work/apache/stratos/code/master/work/stratos"
+echo $path
+
+cp $path/products/stratos/modules/distribution/target/apache-stratos-4.1.0-SNAPSHOT.zip apache-stratos-4.1.0-SNAPSHOT.zip 
 rm -rf apache-stratos-4.1.0-SNAPSHOT
-unzip apache-stratos-4.1.0-SNAPSHOT.zip
+unzip apache-stratos-4.1.0-SNAPSHOT.zip 
 
-#cp $path/components/org.apache.stratos.autoscaler/target/org.apache.stratos.autoscaler-4.1.0-SNAPSHOT.jar apache-stratos-4.1.0-SNAPSHOT/repository/components/plugins/org.apache.stratos.autoscaler_4.1.0.SNAPSHOT.jar
+cp $path/components/org.apache.stratos.autoscaler/target/org.apache.stratos.autoscaler-4.1.0-SNAPSHOT.jar apache-stratos-4.1.0-SNAPSHOT/repository/components/plugins/org.apache.stratos.autoscaler_4.1.0.SNAPSHOT.jar
 
 #cp $path/products/stratos/modules/distribution/src/main/conf/drools/scaling.drl apache-stratos-4.1.0-SNAPSHOT/repository/conf/drools/scaling.drl
 
 cp co/* apache-stratos-4.1.0-SNAPSHOT/repository/conf/
+
